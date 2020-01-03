d4adf61d9404799cb49488a6c346a2b95094e098
dizwe <dizwe2716@gmail.com>
2020-01-04 01:15:24 +0900

    200104 1 Basic cout

---
+++ everyday/bak_2557/bak_2557.cpp
@@ -0,0 +1,12 @@
+// C++ 기초
+#include <iostream>
+
+using namespace std; // 컴파잉ㄹ러 호출
+
+// 함수안에 써야 함 expected constructor, destructor, or type conversion before '<<' token
+int main(){
+    // namespace std 써서 std::cout << "Hello world" 안해도 됨
+    cout << "Hello World!";
+    // cin >> b;
+    return 0;
+}
