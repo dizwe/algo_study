b262e140eb6603efd3ba22aa85b221be714747c2
dizwe <dizwe2716@gmail.com>
2020-02-26 00:07:44 +0900

    sam_sw_2382_미생물격리 MODIFY  코드 반복 되는 것 reference로 해결

--- sam_sw/sam_sw_2382_미생물격리/sam_sw_2382_미생물격리/main.cpp
+++ sam_sw/sam_sw_2382_미생물격리/sam_sw_2382_미생물격리/main.cpp
@@ -61,39 +61,37 @@ void simulate(){
             // 일단 cell 옮기기
             // 이 때는 vector에 하나밖에 없음
             cell_info this_cell = cell_map[one_cell.first][one_cell.second][0];
+            vector< struct cell_info > &this_cell_vector =cell_map[one_cell.first][one_cell.second];
             pair<int, int> next_cord = next_pair(one_cell,this_cell.cell_dir);
             // 있던 애는 빼고 전체는 지우면 안됨!(혹시 뒤에 들어왔던 애가 있을수도 있으니까)
             // erase하면
-            cell_map[one_cell.first][one_cell.second].erase(cell_map[one_cell.first][one_cell.second].begin());
+            this_cell_vector.erase(this_cell_vector.begin());
             // 옮길 자리에 emplace_back(여러개 들어갈 수 있으니까) ?? 근데 옮긴 자리에 누가 있으면?? -> 일단 emplace_back하니까 하나만 옮겨질거라서 괜춘!
             cell_map[next_cord.first][next_cord.second].emplace_back(this_cell);
             next_cell_list.emplace(next_cord); // set은 emplace래
         }
         
-        // 옮기고 나서 합치기(set이라서 중복은 피함)!!!! set
+        // 옮기고 나서 합치기(set이라서 중복은 피함) !!!! set
         for(pair<int, int> one_cell:next_cell_list){
+            vector< struct cell_info > &this_cell_vector =cell_map[one_cell.first][one_cell.second];
             // 약품 발린데 있는지(여기서는 여러개가 갈수가 없으니 하나만 생각하면 될듯
             if(is_inside_medicine(one_cell)==true){
-                cell_map[one_cell.first][one_cell.second][0].cell_organ_num /= 2;
+                this_cell_vector[0].cell_organ_num /= 2;
                 // reference로 좀 더 간단하게 바꿀 방법?
-                if(cell_map[one_cell.first][one_cell.second][0].cell_dir==1){
-                    cell_map[one_cell.first][one_cell.second][0].cell_dir = 2;
-                }else if(cell_map[one_cell.first][one_cell.second][0].cell_dir==2){
-                    cell_map[one_cell.first][one_cell.second][0].cell_dir = 1;
-                }else if(cell_map[one_cell.first][one_cell.second][0].cell_dir==3){
-                    cell_map[one_cell.first][one_cell.second][0].cell_dir = 4;
-                }else if(cell_map[one_cell.first][one_cell.second][0].cell_dir==4){
-                    cell_map[one_cell.first][one_cell.second][0].cell_dir = 3;
+                if(this_cell_vector[0].cell_dir%2==1){
+                    this_cell_vector[0].cell_dir ++;
+                }else if(this_cell_vector[0].cell_dir%2==0){
+                    this_cell_vector[0].cell_dir --;
                 }
             }
             // 아니면 여러개 겹치는데 있는지
             // 한번 하고 나면 사이즈가 1개가 되니까 넘어가겠지?
             
-            if(cell_map[one_cell.first][one_cell.second].size()>1){
+            if(this_cell_vector.size()>1){
                 int max_num = 0;
                 int total_num = 0;
                 int next_dir =0;
-                for(cell_info will_unite_cell:cell_map[one_cell.first][one_cell.second]){
+                for(cell_info will_unite_cell:this_cell_vector){
                     total_num += will_unite_cell.cell_organ_num;
                     if(will_unite_cell.cell_organ_num >max_num){
                         next_dir = will_unite_cell.cell_dir;
@@ -101,8 +99,8 @@ void simulate(){
                     }
                 }
                 // 하나로 합치기
-                cell_map[one_cell.first][one_cell.second].clear();
-                cell_map[one_cell.first][one_cell.second].emplace_back(cell_info{
+                this_cell_vector.clear();
+                this_cell_vector.emplace_back(cell_info{
                     one_cell.first, one_cell.second,
                     total_num, next_dir
                 });
