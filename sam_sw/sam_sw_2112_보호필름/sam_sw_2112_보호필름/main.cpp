be092b957aaec2e82bb6d58fe1df164737f25f6b
dizwe <dizwe2716@gmail.com>
2020-03-18 02:51:04 +0900

    sam_sw_2112_보호필름 dfs 깊이까지 들어가되, 찾았던 깊이는 더 방문하지 않는 것으로 해결 dfs를 bfss 처럼 풀려고 해서 중복이 생겨 시간초과가 났었다.

--- sam_sw/sam_sw_2112_보호필름/sam_sw_2112_보호필름/main.cpp
+++ sam_sw/sam_sw_2112_보호필름/sam_sw_2112_보호필름/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <climits>
 #define EMPTY 99
 #define PASS true
 #define MAX_WIDTH 20 + 1
@@ -14,9 +15,8 @@ bool find_or_not = false;
 short film[MAX_DEPTH][MAX_WIDTH];
 short film_test[MAX_DEPTH][MAX_WIDTH];
 short med_info[MAX_DEPTH]; // 몇번 약을 썼는지도 적어줘야 함
-
-void find_min_med2(int med_num, int current_idx, int med);
-void find_min_med(int med_max_num, int current_idx, int current_med_num,int med);
+int min_med = INT_MAX;
+void find_min_med(int med_max_num, int current_idx, int current_med_num);
 
 bool check_film();
 // 보통 딱 마지막거 하나 틀렸으면 그건 기초적인거 하나 빠진거래!
@@ -28,89 +28,62 @@ int main(){
     for(int t=1;t<=T;t++){
         cin >> DEPTH >> WIDTH >> K;
         
-//        film.resize(DEPTH);
         for(int d =0; d<DEPTH; d++){
-//            film[d].resize(WIDTH);
             for(int w =0; w<WIDTH; w++){
                 cin >> film[d][w];
                 film_test[d][w] = film[d][w];
             }
         }
         
-//        film_test = film;
-        
         // !! 처음에 0이 계속 거슬리는거니까 따로 체크
         if(K==1||check_film()==PASS){
             cout<< "#" << t << " " << 0 << "\n";
         }else{
             // 처음에 resize
-            // !! 문제 : 단일 약품이 아닐때는 처리가 안되는 문제!!(약을 ㅅ
-            for(int max_med=1; max_med<=K; max_med++){
-//                med_info.clear();
-//                med_info.resize(DEPTH, EMPTY);
-                memset(med_info, EMPTY, sizeof(med_info));
-                find_min_med(max_med,0,1,0);
-                //med_info 초기화하고 다른약으로 테스트
-//                med_info.clear();
-//                med_info.resize(DEPTH, EMPTY);
-                memset(med_info, EMPTY, sizeof(med_info));
-                find_min_med(max_med,0,1,1);
-                
-                if(find_or_not==true){
-                    cout<< "#" << t << " " << max_med << "\n";
-                    break;
-                }
-            }
+            fill(&med_info[0], &med_info[MAX_DEPTH] , EMPTY);
+//            memset(med_info, EMPTY, sizeof(med_info));
+            find_min_med(K,0,1);
+            cout<< "#" << t << " " << min_med << "\n";
         }
-        
-        memset(film, EMPTY, sizeof(film));
-        memset(film_test, EMPTY, sizeof(film_test));
-//        film.clear();
-//        film_test.clear();
-//        med_info.clear();
-        memset(med_info, EMPTY, sizeof(med_info));
-        find_or_not=false;
+        // memset 하면 안됨!-> 길원 설명 보기
+        fill(&film[0][0], &film[MAX_DEPTH-1][MAX_WIDTH] , EMPTY);
+        fill(&film[0][0], &film[MAX_DEPTH-1][MAX_WIDTH] , EMPTY);
+        fill(&med_info[0], &med_info[MAX_DEPTH] , EMPTY);
+        min_med = INT_MAX;
     }
     return 0;
 }
 
 
 // med_num이 for loop돌면서 늘어나는 컨셉!
-void find_min_med(int med_max_num, int current_idx, int current_med_num,int med){
-    if(find_or_not==true) return; // !!recursion 돌다가 다 끝나면 끝내야지!
-    // med_num 개수만큼 늘릴 수 있게
-    med_info[current_idx] = med;
-    // med_info에 따라 film 바꾸기
-    for(int c=0; c<WIDTH; c++)
-        film_test[current_idx][c] = med;
-    
-    // 합격하는지 확인하기
-    // 딱 그 개수일때만 체크하면 되잖아
-    if(current_med_num==med_max_num&&check_film()==PASS){ // 합격 하면 break
-//        cout << med_max_num;
-        // !! recursion에서 find를 return 하면 문제 있겠지?
-        find_or_not = true;
-        return;
-    }else{ // 합격 안하면 K보다 작으면 한 depth 더해서 확인하기
-        // med_num까지는 recursion 돌면서 체크!
-        // 모든 값이 다 이상하게 가지는 않는지 확인하자!!!! (특히 값이 증가하는것들)
-        if(current_med_num < med_max_num && current_idx<DEPTH-1){
-            find_min_med(med_max_num, current_idx+1, current_med_num+1, 0);
-            find_min_med(med_max_num, current_idx+1, current_med_num+1, 1);
+// 여기 백트래킹의 문제 : 완전 깊이 들어가면서 하나씩 빼는게 아니라 거기의 index도 조정되어야 한다..
+void find_min_med(int med_max_num, int current_idx, int current_med_num){
+    // !! EMPTY를 한 원소로 했을때 생기는 문제: 0을 굳이 체크할 필요가 있나..?(0 99 는 이미 한번 체크 한건데 또 체크하는 문제 생김)
+    // 시작점 조절하는
+    // !! recursion으로 안하고 이렇게 하면 중복은 없어지곗지?
+    for(int idx=current_idx;idx<DEPTH;idx++){
+        for(int med =0; med<=1;med++){
+            // med_num 개수만큼 늘릴 수 있게
+            med_info[idx] = med;
+            // med_info에 따라 film 바꾸기
+            for(int c=0; c<WIDTH; c++)
+                film_test[idx][c] = med;
+
+            // 깊이 테스트
+            if(check_film() == PASS && current_med_num<min_med){
+                min_med=current_med_num;
+            }else{
+                // next_med_num가 min_medㅂ다 커지면 사실 할 필요도 없다.
+                if(current_med_num+1<min_med)
+                    find_min_med(med_max_num, idx+1, current_med_num+1);
+            }
+            
+            // med_num 개수만큼 늘릴 수 있게
+            med_info[idx] = EMPTY;
+            // 다시 되돌리기
+            for(int c=0; c<WIDTH; c++)
+                film_test[idx][c] = film[idx][c];
         }
-             
-    }
-    
-    // 안되면 다음거!
-    med_info[current_idx] = EMPTY;
-    // 원래대로 되돌리기
-    for(int c=0; c<WIDTH; c++)
-        film_test[current_idx][c] = film[current_idx][c];
-    // 그러고 다음 idx로 항해~~
-    // 얘는 med_num 자체는 안느니까 그대로!
-    if(current_idx+1<DEPTH){
-        find_min_med(med_max_num, current_idx+1, current_med_num, 0);
-        find_min_med(med_max_num, current_idx+1, current_med_num, 1);
     }
         
 }
@@ -137,6 +110,49 @@ bool check_film(){
     return PASS;
 }
 
+
+
+//// med_num이 for loop돌면서 늘어나는 컨셉!
+//void find_min_med3(int med_max_num, int current_idx, int current_med_num,int med){
+//    if(find_or_not==true) return; // !!recursion 돌다가 다 끝나면 끝내야지!
+//    // med_num 개수만큼 늘릴 수 있게
+//    med_info[current_idx] = med;
+//    // med_info에 따라 film 바꾸기
+//    for(int c=0; c<WIDTH; c++)
+//        film_test[current_idx][c] = med;
+//
+//    // 합격하는지 확인하기
+//    // 딱 그 개수일때만 체크하면 되잖아
+//    if(current_med_num==med_max_num&&check_film()==PASS){ // 합격 하면 break
+////        cout << med_max_num;
+//        // !! recursion에서 find를 return 하면 문제 있겠지?
+//        // current_med_num 계산
+//        // !!여기서 문제 깊이 먼저 들어가니까 또 문제 생김
+//        find_or_not = true;
+//        return;
+//    }else{ // 합격 안하면 K보다 작으면 한 depth 더해서 확인하기
+//        // med_num까지는 recursion 돌면서 체크!
+//        // 모든 값이 다 이상하게 가지는 않는지 확인하자!!!! (특히 값이 증가하는것들)
+//        if(current_med_num < med_max_num && current_idx<DEPTH-1){
+//            find_min_med(med_max_num, current_idx+1, current_med_num+1, 0);
+//            find_min_med(med_max_num, current_idx+1, current_med_num+1, 1);
+//        }
+//
+//    }
+//
+//    // 안되면 다음거!
+//    med_info[current_idx] = EMPTY;
+//    // 원래대로 되돌리기
+//    for(int c=0; c<WIDTH; c++)
+//        film_test[current_idx][c] = film[current_idx][c];
+//    // 그러고 다음 idx로 항해~~
+//    // 얘는 med_num 자체는 안느니까 그대로!
+//    if(current_idx+1<DEPTH){
+//        find_min_med(med_max_num, current_idx+1, current_med_num, 0);
+//        find_min_med(med_max_num, current_idx+1, current_med_num, 1);
+//    }
+//
+//}
 //
 //
 //void find_min_med2(int med_num, int current_idx, int med){
