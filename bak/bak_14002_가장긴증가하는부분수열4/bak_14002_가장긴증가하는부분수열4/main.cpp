2831c41a43eea3807e920123cf2d6ec8425c35f3
dizwe <dizwe2716@gmail.com>
2020-05-22 09:43:47 +0900

    bak_14002 가장긴증가하는부분수열4 제일 마지막 dp가 정답은 아니다

--- bak/bak_14002_가장긴증가하는부분수열4/bak_14002_가장긴증가하는부분수열4/main.cpp
+++ bak/bak_14002_가장긴증가하는부분수열4/bak_14002_가장긴증가하는부분수열4/main.cpp
@@ -27,6 +27,8 @@ int main(int argc, const char * argv[]) {
     
     dp[0] = 1;
     prev_idx[0] = -1; // this is start
+    int max_idx = 0;
+    int max_len = 1;
     
     for(int i=1;i<N; i++){
         int len = 1; // 나는 포함이 되니까?
@@ -44,23 +46,30 @@ int main(int argc, const char * argv[]) {
             prev_idx[i] = -1;
         }else{
             dp[i] = len;
+            if(len > max_len){ // 최대값이 아닐수도 있기 때문에!
+                max_len = len;
+                max_idx = i;
+            }
         }
     }
-    
+
     // lower bound를 쓰기도 하네..
     // !! prev 하면 거꾸로 되니까 거꾸로 적을것도 생각해야 한다..
-    cout << dp[N-1] << "\n";
-    int k = N-1;
-    int i = dp[N-1]-1; // 지금 들어갈 개수
+    // !! 중요한걸 간과했따!! 무조건 마지막게 최대라는 보장은 없다...
+    // 예전에는 자주 프린트 했는데 그런 습관도...
+    cout << dp[max_idx] << "\n";
+    int k = max_idx;
+    int i = dp[max_idx]-1; // 지금 들어갈 개수
     while(prev_idx[k] !=-1){
         shortest_way[i--]= row[k];
         k = prev_idx[k];
     }
     shortest_way[0] = row[k];
     
-    for(int i=0 ; i<dp[N-1] ;i++){
+    for(int i=0 ; i< dp[max_idx];i++){
         cout << shortest_way[i] << " ";
     }
     
     return 0;
 }
+
